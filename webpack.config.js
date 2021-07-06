c1674c5f0a9c5f59fa86f08ea7ff2aef594f5f97
Matej Hakoš <matejhakos@gmail.com>
2021-07-06 17:47:50 +0200

    testing workflow

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        filename: 'bundle.js',
+        filename: 'thingy.js',
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
