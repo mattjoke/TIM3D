652ef42968b8b45f98a3b46d7e53572dd3c54a42
Matej Hakoš <36647232+mattjoke@users.noreply.github.com>
2022-03-07 00:24:10 +0100

    src directory fix

--- src/Factory.ts
+++ src/Factory.ts
@@ -15,7 +15,7 @@ import { Object3D } from 'stuff/Object3D';
 import { v4 as uuidv4 } from 'uuid';
 
 /**
- * Factory of instacn
+ * Factory of instance.
  * @author Matej Hakoš
  *
  * @class Factory
