efde49602935f1fcacd58084d810c7f6dd1b63e1
mattjoke <36647232+mattjoke@users.noreply.github.com>
2022-05-16 20:49:15 +0000

    Deploying to gh-pages-build from @ mattjoke/TIM3D@0de2ec168018c659475bf864dfa0c148996b89d2 🚀

--- docs/scripts/src/loaders/loaders/loaderOverlay.d.ts
+++
@@ -1,9 +0,0 @@
-import '../loaders/loader.css';
-/**
- * Creates overlay div and plays loading animation.
- * @author Matej Hakoš
- *
- * @return {HTMLDivElement}
- */
-declare const loaderOverlay: () => HTMLDivElement;
-export { loaderOverlay };
