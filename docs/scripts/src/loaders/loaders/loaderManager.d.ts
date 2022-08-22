115535ddc7b714019cefdf10f75577e6fee0393d
mattjoke <36647232+mattjoke@users.noreply.github.com>
2022-08-22 09:20:47 +0000

    Deploying to gh-pages-build from @ mattjoke/TIM3D@eefb9f942a2e3c860f1a1e3b2543b5b4e51863b5 🚀

---
+++ docs/scripts/src/loaders/loaders/loaderManager.d.ts
@@ -0,0 +1,9 @@
+import { LoadingManager } from 'three';
+/**
+ * Sets up loading managers supported by Three.js
+ * @author Matej Hakoš
+ *
+ * @return {LoadingManager}
+ */
+declare const loaderManager: () => LoadingManager;
+export { loaderManager };
