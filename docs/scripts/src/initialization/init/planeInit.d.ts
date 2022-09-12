96f06126738cfbccf5a202ee20f37fe6346e9ae1
mattjoke <36647232+mattjoke@users.noreply.github.com>
2022-09-12 10:08:40 +0000

    Deploying to gh-pages-build from @ mattjoke/TIM3D@d64a8121100a10fd6660d90518e99d4133c63e2d 🚀

--- docs/scripts/src/initialization/init/planeInit.d.ts
+++
@@ -1,8 +0,0 @@
-import { AmbientLight, PointLight } from 'three';
-/**
- * Initializes plane with default ambient and point light color.
- *
- * @return {[AmbientLight, PointLight]}
- */
-declare const planeInit: () => (AmbientLight | PointLight)[];
-export { planeInit };
