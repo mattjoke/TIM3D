e277974e33a1e84edf997717f26abade0214f50d
mattjoke <36647232+mattjoke@users.noreply.github.com>
2022-09-12 10:11:58 +0000

    Deploying to gh-pages-build from @ mattjoke/TIM3D@29a9dc4ca7e5f65fb56208e6e0bf6762d64044cf 🚀

---
+++ docs/scripts/prod/src/utils/Utils.d.ts
@@ -0,0 +1,50 @@
+import { ComputedPostions, ObjectID } from '../types/applicationTypes';
+import { Color } from 'three';
+/**
+ * Checks if input is parsable color.
+ * @author Matej Hakoš
+ *
+ * @param {(string | Color)} color
+ * @return {boolean}
+ */
+declare const isColor: (color: string | Color) => boolean;
+/**
+ * Parse file extension from file path.
+ * @author Matej Hakoš
+ *
+ * @deprecated
+ * @param {ObjectID} filename
+ * @return {string}
+ */
+declare const parseExtension: (filename: ObjectID) => string | undefined;
+/**
+ * Checks if document is already in fullscreen.
+ * (Note: @ts-ignore is required because of unsupported types.)
+ * @author Matej Hakoš
+ *
+ * @param {?Document} [document]
+ * @return{boolean}
+ */
+declare const checkFullscreen: (document?: Document) => any;
+/**
+ * Generated (pseudo)random color.
+ * @author Matej Hakoš
+ *
+ * @return {number}
+ */
+declare const generateRandomColor: () => number;
+/**
+ * Generates random color from list of known css colors.
+ * @author Matej Hakoš
+ *
+ * @return {string}
+ */
+declare const generateRandomSeededColor: () => string;
+/**
+ * Creates a deep copy of Map.
+ *
+ * @param {ComputedPostions} obj
+ * @return {ComputedPostions}
+ */
+declare const deepCopyMap: (obj: ComputedPostions) => ComputedPostions;
+export { isColor, parseExtension, checkFullscreen, deepCopyMap, generateRandomColor, generateRandomSeededColor };
